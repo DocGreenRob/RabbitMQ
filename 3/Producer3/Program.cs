97fceed586f701dd7b35fe209fdacf2937ed1d12
Robert Green <robert.green.it.2017@gmail.com>
2020-12-23 18:59:50 -0500

    3

---
+++ 3/Producer3/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer3
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			var message = GetMessage(args);
+			for (var i = 0; i < 10; i++)
+			{
+				PublishMessage($"[{i + 1}] :: {message}");
+			}
+		}
+
+		private static void PublishMessage(string message)
+		{
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
+
+				var body = Encoding.UTF8.GetBytes(message);
+				channel.BasicPublish(exchange: "logs", routingKey: "", basicProperties: null, body: body);
+				Console.WriteLine(" [x] Sent {0}", message);
+			}
+
+			//Console.WriteLine(" Press [enter] to exit.");
+			//Console.ReadLine();
+		}
+
+		private static string GetMessage(string[] args)
+		{
+			return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+		}
+	}
+}
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+https://www.rabbitmq.com/tutorials/tutorial-three-dotnet.html
+publish/subscribe
+- published messages are going to be broadcast to all the receivers
