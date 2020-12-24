1a6f44cf6e5718bafa0e734dbbf14edbe7b38f98
Robert Green <robert.green.it.2017@gmail.com>
2020-12-23 22:01:31 -0500

    5

---
+++ 5/Producer5/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Producer5
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+			using (var connection = factory.CreateConnection())
+			using (var channel = connection.CreateModel())
+			{
+				channel.ExchangeDeclare(exchange: "topic_logs",
+										type: ExchangeType.Topic);
+
+				var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+				var message = (args.Length > 1)
+							  ? string.Join(" ", args.Skip(1).ToArray())
+							  : "Hello World!";
+				var body = Encoding.UTF8.GetBytes(message);
+				channel.BasicPublish(exchange: "topic_logs",
+									 routingKey: routingKey,
+									 basicProperties: null,
+									 body: body);
+				Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+			}
+		}
+	}
+}
